c6ea5c0f7fc88cddb22e76d2e2f716bfa7588cd0
rivannadev <nacho.rivero.dev@gmail.com>
2019-11-09 22:08:56 +0100

    commit

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const path = require("path")
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+
+const postRoutes = require("./routes/post");
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public"))); //static files management
+
+app.use("/api/post", postRoutes);
 
 app.listen(8000, () => {
     console.log("Listening"); //In order to cancel long listening
--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const postController = require ('../controllers/postColler')
 router.get('/', postController.index) //first endpoint get slash and post controller
 
 module.exports = router;
